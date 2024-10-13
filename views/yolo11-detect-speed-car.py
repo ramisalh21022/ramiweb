6be8ff578b500899569ad88e4533b8db00a89399
ramisalh21022 <134399309+ramisalh21022@users.noreply.github.com>
2024-10-13 11:06:47 -0400

    Add files via upload

---
+++ views/yolo11-detect-speed-car.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title("yolo11-detect-speed-car")
+img=st.video("assets/yolo11-detect-speed-car.mp4")
+st.write("download from :{https://github.com/ramisalh21022/yolo11-detect-speed-car}")
