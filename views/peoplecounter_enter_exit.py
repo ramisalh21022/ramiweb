bbb07a4c095b03b8916faa5bc741e91802d8247c
ramisalh21022 <134399309+ramisalh21022@users.noreply.github.com>
2024-10-23 09:03:17 -0400

    Add files via upload

---
+++ views/peoplecounter_enter_exit.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title("peoplecounter_enter_exit")
+img=st.video("assets/peoplecounter_enter_exit.mp4")
+st.write("download from :{https://github.com/ramisalh21022/peoplecounter_enter_exit}")
