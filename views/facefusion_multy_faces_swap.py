4a11a1168628f96071672ea0ffc28cf79208567c
ramisalh21022 <134399309+ramisalh21022@users.noreply.github.com>
2024-11-13 04:09:11 -0500

    Update facefusion_multy_faces_swap.py

--- views/facefusion_multy_faces_swap.py
+++ views/facefusion_multy_faces_swap.py
@@ -5,10 +5,10 @@ col1,col2=st.columns(2,gap='small',vertical_alignment="top")
 with col1:
     #st.image("assets/rami1-modified.png",width=230)
     st.title("befor_swap", anchor=False)
-    st.video("assets/f1.mp4")
+    st.video("assets/face1.mp4")
 with col2:
     st.title("after_swap", anchor=False)
-    st.video("assets/f2.mp4")
+    st.video("assets/face2.mp4")
     #st.write("Electronic engineer, oracl databse sql/plsql,postgresdb ,python computer vision detection object ,developer flutter.📞+963933210196 -📧ramisalh21022.gmail.com")    
     
 st.write("download from :{https://github.com/ramisalh21022/ramifaces}")
