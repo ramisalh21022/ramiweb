69f0b83aaae09f7fd2422a5ce678a33efc0f914e
ramisalh21022 <134399309+ramisalh21022@users.noreply.github.com>
2025-08-16 23:30:11 -0700

    Update pet-stor-flutter-supabase.py

--- views/pet-stor-flutter-supabase.py
+++ views/pet-stor-flutter-supabase.py
@@ -1,4 +1,5 @@
 import streamlit as st
 st.title("application-mobil-web-for-pet-store-in-flutter-supabase")
-img=st.video("assets/pet-stor-flutter-supabase.mp4")
+img=st.video("assets/pet-stor.mp4")
+
 st.write("download from :{https://github.com/ramisalh21022/pet-stor-flutter-supabase}")
