7ec9582e44e2230a207164fe0926e0fe3a306db4
ramisalh21022 <134399309+ramisalh21022@users.noreply.github.com>
2025-08-16 23:22:21 -0700

    Add files via upload

---
+++ views/education-app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title("education-app-store-in-flutter-database")
+img=st.video("assets/education_mobil.mp4")
+st.write("download from :{https://github.com/ramisalh21022/education-app}")
